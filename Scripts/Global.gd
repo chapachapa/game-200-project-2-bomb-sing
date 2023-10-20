ed9f28cb6c21dff3620f3c50bd0d52b06c06f584
Charles Park <charlesjypark@gmail.com>
2023-10-20 02:34:15 -0700

    Got to pitch detection to work
    But needs some ironing out to make it properly work.

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -6,6 +6,10 @@ var volume = 0.0
 var energy = 0.0
 var magnitude = 0.0
 var spectrum_analyzer: AudioEffectSpectrumAnalyzerInstance
+const MAX_FREQ = 5000;
+const MIN_FREQ = 0;
+const FREQ_INTERVAL = 10;
+var magnitueds_by_range = [];
 
 
 func _ready():
@@ -17,8 +21,24 @@ func _process(delta):
 	volume = db_to_linear(AudioServer.get_bus_peak_volume_left_db(3, 0))
 	
 	# find the pitch
-	magnitude = linear_to_db(spectrum_analyzer.get_magnitude_for_frequency_range(0, 200).length())
+	# magnitude = linear_to_db(spectrum_analyzer.get_magnitude_for_frequency_range(0, 200).length())
+	var max = 0;
+	var mIndex = -1;
+	for n in floor((MAX_FREQ - MIN_FREQ) / FREQ_INTERVAL):
+		var freq = MIN_FREQ + (FREQ_INTERVAL * n);
+		var intervalMagnitudeVector = spectrum_analyzer.get_magnitude_for_frequency_range(
+			freq,
+			freq+FREQ_INTERVAL, 
+			AudioEffectSpectrumAnalyzerInstance.MAGNITUDE_AVERAGE);
+		var intervalMagnitudeAverage = intervalMagnitudeVector.x + intervalMagnitudeVector.y / 2;
+		if intervalMagnitudeAverage > max:
+			mIndex = n;
+			max = intervalMagnitudeAverage;
+	print("::::::::::::::::::::: highest in range is .... ", mIndex, " ... ", mIndex / 30.0);
+	mIndex;
+		
 	
 	# boost the signal and normalize it
-	energy = clamp((MIN_DB + (magnitude))/MIN_DB, 0, 1)
+	#energy = clamp((MIN_DB + (magnitude))/MIN_DB, 0, 1)
+	energy = mIndex / 30.0;
 	print(energy)
