a6aa983610b6a4df089a7ea14924f07adb8b4f8f
Sam Feng <fengyuan20000303@gmail.com>
2023-10-24 23:28:01 -0700

    Audio Reactive Walls

--- scenes/wall.gd
+++ scenes/wall.gd
@@ -2,8 +2,15 @@ extends StaticBody2D
 
 @export var moving = false
 @export var move_beat = 0
-var targets = []
-var index
+var index = 0
+var positions = []
+
+
+func _ready():
+	for i in range(1, 3):
+		positions.append(get_node("Location%d" % i).get_global_position())
+		print(get_node("Location%d" % i).get_position())
+
 
 func _physics_process(delta):
 	pass
@@ -15,7 +22,8 @@ func on_beat(beat):
 	tween.tween_property($Sprite, "scale", Vector2(1, 1), 0.2)
 	
 	tween.set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
-	tween.parallel().tween_property(self, "position", get_position() + Vector2(0, 20), 0.2)
+	tween.parallel().tween_property(self, "position", positions[index], 0.2)
+	index = wrapi(index + 1, 0, positions.size())
 
 
 func on_bar(bar):
